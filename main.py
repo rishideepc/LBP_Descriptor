e6a6fbb8cabacf6a0b1f37addf934128d4e0078c
rishideepc <rishi1a2@gmail.com>
2023-12-01 17:29:45 +0530

    Recent updates

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 from skimage.feature import local_binary_pattern
 from sklearn.svm import LinearSVC, SVC
 from sklearn.model_selection import train_test_split
+from sklearn import metrics
 import numpy as np
 import os
 
@@ -11,19 +12,10 @@ n_points= 8 * radius
 region_size= 20
 threshold_factor = 0.3
 labels= []
+scaling_factor = 2.0
 
 
 def divide_into_regions(_lbp, _region_size):
-    """
-    Dividing LBP images into non-overlapping regions
-
-    @param:
-        _lbp -  the LBP feature image, that is, matrix that contains the LBP values for each pixel in the original image).
-        _region_size - specifies the size of the non-overlapping regions into which the LBP feature image will be divided for texture recognition.
-
-    @returns:
-        regions - nested list that represents the divided image.
-    """
     height, width = _lbp.shape
     regions = []
     for i in range(0, height, _region_size):
@@ -33,18 +25,21 @@ def divide_into_regions(_lbp, _region_size):
     return regions
 
 
-def threshold_histogram(_histogram, _threshold_factor):
+def normalize_histogram(_histogram):
     """
-    Computing threshold for each region histogram
+    Normalize the histogram to have unit L1 norm.
 
     @param:
         _histogram - the histogram of LBP values for a given region of an image.
-        _threshold_factor - the fraction of the maximum histogram value to use as the threshold.
 
     @returns:
-        _histogram - the thresholded histogram as a binary array.
+        _histogram - the normalized histogram.
     """
+    return _histogram / np.sum(_histogram)
 
+
+def threshold_histogram(_histogram, _threshold_factor):
+    _histogram = normalize_histogram(_histogram)
     threshold = _threshold_factor * np.mean(_histogram)
     _histogram[_histogram < threshold] = 0
     _histogram[_histogram >= threshold] = 1
@@ -53,61 +48,78 @@ def threshold_histogram(_histogram, _threshold_factor):
 
 if __name__=="__main__":
 
-    #   Loading image dataset
     dataset_path=  "assets/textures/"
     images= []
     for filename in os.listdir(dataset_path):
         img= cv2.imread(os.path.join(dataset_path, filename))
-        img= cv2.resize(img, dimensions)
+        # img= cv2.resize(img, dimensions)
         images.append(img)
 
+    cv2.imshow('original image', images[0])
 
-    # images= np.array(images)
 
+    ##############################################################
 
-    #   Converting images to grayscale
     gray_images= []
     for img in images:
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         gray_images.append(gray)
 
-    # gray_images= np.array(gray_images)
+    cv2.imshow('grayscale image', gray_images[0])
 
-    
-    #   Extracting LBP features from grayscale images
-    lbp_images= []
+
+    ##############################################################
+
+    filtered_images=[]
     for gray in gray_images:
-        lbp = local_binary_pattern(gray, n_points, radius, method='uniform')
+        filtered= cv2.medianBlur(gray, 5)
+        filtered_images.append(filtered)
+
+    cv2.imshow('filtered image', filtered_images[0])
+
+
+    ##############################################################
+
+    lbp_images= []
+    for filtered in filtered_images:
+        lbp = local_binary_pattern(filtered, n_points, radius, method='uniform')
         lbp_images.append(lbp)
 
-    # lbp_images= np.array(lbp_images)
+    cv2.imshow('lbp image', lbp_images[0])
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
+    ##############################################################
 
-    #   Applying noise-resistant thresholding to LBP images
     thresholded_images= []
-    #   Looping through each lbp image
     for lbp in lbp_images:
         regions= divide_into_regions(lbp, region_size)
         thresholded_regions= []
-        #   Looping through each region of an lbp image
         for region in regions:
             histogram, _= np.histogram(region, bins=np.arange(0, 10), density=True)
             histogram = threshold_histogram(histogram, threshold_factor)
-            # print(histogram)
             thresholded_regions.append(histogram)
         thresholded_image= np.concatenate(thresholded_regions)
         thresholded_images.append(thresholded_image)
-    thresholded_images= np.array(thresholded_images) # feature list
+
+    cv2.imshow('region', region[0])
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    thresholded_images= np.array(thresholded_images)
+
+    
+
+    ###############################################################
+  
     for i in range(len(thresholded_images)):
         labels.append(f'texture{i+1}')
     labels= np.array(labels)
-    
 
-    #   Classifying the Texture Pattern
-    X_train, X_test, y_train, y_test = train_test_split(thresholded_images, labels, test_size=0.3)
+    X_train, X_test, y_train, y_test = train_test_split(thresholded_images, labels, test_size=0.15)
     model = LinearSVC()
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
-
     print('\n', y_pred, "\n")
+
+
