5aca4a9d4c343a66193ee4ddc8714c769fb83c9f
rishideepc <rishi1a2@gmail.com>
2023-12-02 09:25:06 +0530

    Added relevant comments and removed filtering and preprocessing

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+##### Importing relevant libraries ##############################
 import cv2
 from skimage.feature import local_binary_pattern
 from sklearn.svm import LinearSVC, SVC
@@ -5,16 +6,28 @@ from sklearn.model_selection import train_test_split
 from sklearn import metrics
 import numpy as np
 import os
+################################################################
 
+############################# Data #############################
+# image resizing variables
 dimensions = (64, 64)
-radius= 1                                                   
-n_points= 8 * radius
-region_size= 20
-threshold_factor = 0.3
-labels= []
 scaling_factor = 2.0
 
+# local binary pattern @param
+radius= 1                                                   
+n_points= 8 * radius
 
+# thresholding @param
+region_size= 20
+threshold_factor = 0.3
+
+# model training
+labels= []
+################################################################
+
+
+########################## Helper functions ####################
+# Method to divide LBP image in non-overlapping regions
 def divide_into_regions(_lbp, _region_size):
     height, width = _lbp.shape
     regions = []
@@ -23,29 +36,25 @@ def divide_into_regions(_lbp, _region_size):
             region= _lbp[i:i+_region_size, j:j+_region_size]
             regions.append(region)
     return regions
+#######################################################
 
-
+# Method to normalize LBP histogram ###################
 def normalize_histogram(_histogram):
-    """
-    Normalize the histogram to have unit L1 norm.
 
-    @param:
-        _histogram - the histogram of LBP values for a given region of an image.
-
-    @returns:
-        _histogram - the normalized histogram.
-    """
     return _histogram / np.sum(_histogram)
+#######################################################
 
-
+# Method to threshold computed LBP histogram ##########
 def threshold_histogram(_histogram, _threshold_factor):
     _histogram = normalize_histogram(_histogram)
     threshold = _threshold_factor * np.mean(_histogram)
     _histogram[_histogram < threshold] = 0
     _histogram[_histogram >= threshold] = 1
     return _histogram
+#######################################################
 
 
+# Main function #######################################
 if __name__=="__main__":
 
     dataset_path=  "assets/textures/"
@@ -56,41 +65,40 @@ if __name__=="__main__":
         images.append(img)
 
     cv2.imshow('original image', images[0])
+    # displaying original image for reference
 
 
-    ##############################################################
-
+    ############ Grayscaling & converting image to NxM graylevel array #########
     gray_images= []
     for img in images:
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         gray_images.append(gray)
 
     cv2.imshow('grayscale image', gray_images[0])
+    ###########################################################################
+
+    ######## Filtering grayscale image to further smoothen image ##############
+    # filtered_images=[]
+    # for gray in gray_images:
+    #     filtered= cv2.medianBlur(gray, 5)
+    #     filtered_images.append(filtered)
+
+    # cv2.imshow('filtered image', filtered_images[0])
+    ###########################################################################
 
 
-    ##############################################################
-
-    filtered_images=[]
-    for gray in gray_images:
-        filtered= cv2.medianBlur(gray, 5)
-        filtered_images.append(filtered)
-
-    cv2.imshow('filtered image', filtered_images[0])
-
-
-    ##############################################################
-
+    ## Extracting LBP features from Grayscale image & storing in feature map ##
     lbp_images= []
-    for filtered in filtered_images:
-        lbp = local_binary_pattern(filtered, n_points, radius, method='uniform')
+    for gray in gray_images:
+        lbp = local_binary_pattern(gray, n_points, radius, method='uniform')
         lbp_images.append(lbp)
 
     cv2.imshow('lbp image', lbp_images[0])
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    ##########################################################################
 
-    ##############################################################
-
+    ##### Further thresholding based on a threshold factor ###################
     thresholded_images= []
     for lbp in lbp_images:
         regions= divide_into_regions(lbp, region_size)
@@ -101,16 +109,11 @@ if __name__=="__main__":
             thresholded_regions.append(histogram)
         thresholded_image= np.concatenate(thresholded_regions)
         thresholded_images.append(thresholded_image)
-
-    cv2.imshow('region', region[0])
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
     thresholded_images= np.array(thresholded_images)
+    #########################################################################
 
-    
 
-    ###############################################################
-  
+    ####### Placeholder code for labeling, model training, and testing ######
     for i in range(len(thresholded_images)):
         labels.append(f'texture{i+1}')
     labels= np.array(labels)
@@ -121,5 +124,8 @@ if __name__=="__main__":
 
     y_pred = model.predict(X_test)
     print('\n', y_pred, "\n")
+    ########################################################################
 
 
+# Normalize within range
+# Remove Filter
